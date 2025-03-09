86d89dc11094b78b0edf6ab77fd7cfd1ad84de59
gauravbisen1 <gouravbisen123@gmail.com>
2025-03-09 18:31:38 +0530

    after 2 days break(collage stuffs!)

---
+++ productA.java
@@ -0,0 +1,10 @@
+public class productA {
+    public static int multiply(int a, int b){
+        int product = a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int prod = multiply(3, 5) ;
+        System.out.println(prod);
+    }
+}
