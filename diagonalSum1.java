7651dde13995c3e164e669ae5caabde6bec530f9
gauravbisen1 <gouravbisen123@gmail.com>
2025-03-26 13:46:46 +0530

    ending of the 2d arrays.

---
+++ diagonalSum1.java
@@ -0,0 +1,29 @@
+public class diagonalSum1 {
+    public static int diagonalSum1(int matrix [][]){
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                //primary diagonal
+                if (i==j) {
+                    sum += matrix[i][j];
+                }
+                //secondary diagonal
+                else if (i+j == matrix.length-1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        diagonalSum1(matrix);
+    }
+}
+//time complexity = 0(n^2)
---
+++ diagonalSum2.java
@@ -0,0 +1,24 @@
+public class diagonalSum2 {
+    public static int diagonalSum2(int matrix [][]){
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            //primary diagonal
+            sum += matrix[i][i];
+            //secondary diagonal
+            if (i!=matrix.length-1-i) {
+                sum += matrix[i][matrix.length-i-1];
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        diagonalSum2(matrix);
+    }
+}
