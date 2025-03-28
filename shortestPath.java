0fac0149913655eeb6d5b8af897a620e9e1f8ba6
gauravbisen1 <gouravbisen123@gmail.com>
2025-03-28 23:59:00 +0530

    shortest path with strings

---
+++ shortestPath.java
@@ -0,0 +1,24 @@
+public class shortestPath{
+    public static float shortestPath(String path){
+        int x=0 , y=0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            if(dir == 'S'){
+                y--;
+            }
+            else if(dir == 'S'){
+                y--;
+            }
+            else if(dir == 'S'){
+                y--;
+            }
+            else{
+                y--;
+            }
+        }
+        //return path;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
