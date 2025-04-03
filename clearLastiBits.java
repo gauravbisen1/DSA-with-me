03e7eaddd61aa57ed56275996f671ad76ab3bf85
gauravbisen1 <gouravbisen123@gmail.com>
2025-04-03 20:48:20 +0530

    end of the bitsss...

---
+++ clearLastiBits.java
@@ -0,0 +1,9 @@
+public class clearLastiBits{
+    public static int clearBits(int n , int i){
+        int bitMask = (~0)<<i;
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(clearBits(15, 2));
+    }
+}
