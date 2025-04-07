dee2f5bcf8227e5793e0dc93cb3e9ef02de0c860
gauravbisen1 <gouravbisen123@gmail.com>
2025-04-07 23:46:51 +0530

    Oops!

---
+++ Oops/get_set.java
@@ -0,0 +1,27 @@
+public class get_set {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("blue");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+        p1.setColor("yellow");
+        System.out.println(p1.getColor());
+    }
+}
+class Pen{
+    private String color;
+    private int tip;
+    String getColor(){
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
+    void setColor(String newColor){
+        this.color = newColor;
+    }
+    void setTip(int Tip){
+        this.tip = Tip;
+    }
+}
