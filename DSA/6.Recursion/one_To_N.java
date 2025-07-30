ac195119535a74f6af574760cf777acf6f897332
gauravbisen1 <gouravbisen123@gmail.com>
2025-07-30 14:00:51 +0530

    problem no. 2

---
+++ DSA/6.Recursion/one_To_N.java
@@ -0,0 +1,15 @@
+public class one_To_N {
+    public static void printInc(int n){
+        //base case
+        if (n==1) {
+         System.out.print(n+" ");  
+         return;  
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printInc(n);
+    }
+}
