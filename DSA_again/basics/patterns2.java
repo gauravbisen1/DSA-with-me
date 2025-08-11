49bbd31d4547d506f4fb92b7ee11cd6740e24607
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-11 12:58:08 +0530

    more patterns2

--- DSA_again/basics/patterns2.java
+++ DSA_again/basics/patterns2.java
@@ -1,6 +1,6 @@
 public class patterns2{
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
         //pattern
         // *****
         //  ****
@@ -45,6 +45,66 @@ public class patterns2{
         //     System.out.println("");
         // }
 
+        //palindrome pattern
+        // for (int i = 1; i <= n; i++) {
+        //     int spaces = n-i;
+            
+        //     for (int j = 1; j <= spaces; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     for (int j = i-1; j >= 1; j--) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println("");
+        // }
+
+        //pattern cross
+        
+        // for (int i = 1; i <= n; i++) {
+        //     int spaces = 2*(n-i);
+        //     int stars = i;
+        //     for (int j = 1; j <= stars; j++) {
+        //         System.out.print("*");
+        //     }
+        //     for (int j = 1; j <= spaces; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= stars; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+        // for (int i = n; i >= 1; i--) {
+        //     int spaces = 2*(n-i);
+        //     int stars = i;
+        //     for (int j = 1; j <= stars; j++) {
+        //         System.out.print("*");
+        //     }
+        //     for (int j = 1; j <= spaces; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= stars; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+
+        //no. pattern
+        int count=1;
+        for (int i = 1; i <= n; i++) {
+            int spaces = n-i;
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println("");
+        }
 
     }
 }
