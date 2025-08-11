ee77b430ae6641130231aa7fdf12f7d85a6d8da6
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-11 12:43:55 +0530

    patterns 2

---
+++ DSA_again/basics/patterns2.java
@@ -0,0 +1,50 @@
+public class patterns2{
+    public static void main(String[] args) {
+        int n = 5;
+        //pattern
+        // *****
+        //  ****
+        //   ***
+        //    **
+        //     *
+        // for (int i = n; i >= 1; i--) {
+        //     int spaces = n-i;
+        //     int stars = i;
+        //     for (int j = 1; j <= spaces; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= stars; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");
+        // }
+
+        //kite pattern
+        // for (int i = 1; i <= n; i++) {
+        //     int spaces = n-i;
+        //     int stars = 2*i-1;
+        //     for (int j = 1; j <= spaces; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= stars; j++) {
+        //         System.out.print("*");
+        //     }
+            
+        //     System.out.println("");
+        // }
+        // for (int i = n; i >= 1; i--) {
+        //     int spaces = n-i;
+        //     int stars = 2*i-1;
+        //     for (int j = 1; j <= spaces; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= stars; j++) {
+        //         System.out.print("*");
+        //     }
+            
+        //     System.out.println("");
+        // }
+
+
+    }
+}
