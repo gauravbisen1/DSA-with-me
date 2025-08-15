de644306592b6a9956de84460083f287eeaea1de
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-15 18:03:29 +0530

    anagrams sol2

--- DSA_again/5strings/anagrams.java
+++ DSA_again/5strings/anagrams.java
@@ -14,9 +14,28 @@ public class anagrams {
 
         return  Arrays.equals(arr1, arr2);
     }
+    public static boolean anagramsTwo(String s, String t){
+        char [] arr1 = s.toCharArray();
+        char [] arr2 = t.toCharArray();
+        int[] count = new int[26]; // counts for a-z
+
+        for (int i = 0; i < arr1.length; i++) {
+            count[arr1[i]-'a']++;
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            count[arr2[i]-'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         String s = "race";
         String t = "care";
         System.out.println(anagramsOne(s, t));
+        System.out.println(anagramsTwo(s, t));
     }
 }
