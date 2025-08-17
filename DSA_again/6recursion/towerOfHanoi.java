dcab8cc2cc54a5d9f535b57b94063b5afb299450
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-17 16:15:13 +0530

    towe of hanoi

---
+++ DSA_again/6recursion/towerOfHanoi.java
@@ -0,0 +1,13 @@
+public class towerOfHanoi {
+    public static void hanoi(int n, char a, char b, char c){
+        if(n == 0){
+            return;
+        }
+        hanoi(n-1, a, c, b);
+        System.out.println("Move disk: " + n + " from " + a + " to " + c);
+        hanoi(n-1, b, a, c);
+    }
+    public static void main(String[] args) {
+        hanoi(3, 'a', 'b', 'c');
+    }
+}
