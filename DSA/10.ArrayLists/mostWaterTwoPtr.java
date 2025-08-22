babeb79e6732ab6b04f53f88a39b67667b88f78c
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-22 12:01:24 +0530

    two pointer approch

---
+++ DSA/10.ArrayLists/mostWaterTwoPtr.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class mostWaterTwoPtr {
+    public static int storeWater(ArrayList<Integer> height){
+        int maxWater = 0;
+        int LP = 0;
+        int RP = height.size()-1;
+
+        while(LP<RP){
+            //calculate water area
+            int ht = Math.min(height.get(LP), height.get(RP));
+            int width = RP - LP;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater,currWater);
+
+            //update pointer
+            if (height.get(LP)<height.get(RP)) {
+                LP++;
+            } else {
+                RP--;
+            }
+        }
+        return maxWater;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        //1,8,6,2,5,4,8,3,7
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println(storeWater(height));
+    }
+}
