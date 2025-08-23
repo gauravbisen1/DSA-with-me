22c16203eefd1aa14c99aa545d81bc3169ed6068
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-24 00:16:01 +0530

    ll

--- DSA/11.LinkedList/LinkedList.java
+++ DSA/11.LinkedList/LinkedList.java
@@ -1,3 +1,5 @@
+
+
 public class LinkedList{
     public static class Node{
         int data;
@@ -76,6 +78,47 @@ public class LinkedList{
         temp.next = newNode;
     }
 
+    // remove first
+    public int removeFirst(){
+        if (size==0) {
+            System.out.println("Linked list is empty.");
+            return Integer.MIN_VALUE;
+        } else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        int val = head.data;
+        size--;
+        head = head.next;
+        return val;
+    }
+
+    // remove last
+    public int removeLast(){
+        if (size==0) {
+            System.out.println("Linked list is empty.");
+            return Integer.MIN_VALUE;
+        } else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev: i=size-2
+        Node prev = head;
+        for (int i = 0; i < size-2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;//tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
     
@@ -84,6 +127,8 @@ public class LinkedList{
         ll.addLast(4);
         ll.addLast(5);
         ll.add(2, 3);
+        ll.removeFirst();
+        ll.removeLast();
 
         ll.print();
         System.out.println(ll.size);
