fc843ad40f2f981dedce3823de21113287d01915
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-23 23:49:01 +0530

    add and size

--- DSA/11.LinkedList/LinkedList.java
+++ DSA/11.LinkedList/LinkedList.java
@@ -10,11 +10,13 @@ public class LinkedList{
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     //add first
     public void addFirst(int data){
         //step1 - create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -29,6 +31,7 @@ public class LinkedList{
     public void addLast(int data){
         //step1 - create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -53,16 +56,36 @@ public class LinkedList{
         System.out.println("null");
     }
 
+    //add in middle
+    public void add(int idx , int data){
+        if (idx==0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx-1) {
+            temp = temp.next;
+            i++;
+        }
+        //i=idx-1; temp->prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.print();
+    
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1);
-        ll.print();
-        ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2, 3);
+
         ll.print();
+        System.out.println(ll.size);
     }
 }
