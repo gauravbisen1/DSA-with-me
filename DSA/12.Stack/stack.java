9a20097fe332cbe6bffa1f8b148c6c34eb39a637
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-28 11:08:08 +0530

    q2 q3

--- DSA/12.Stack/stack.java
+++ DSA/12.Stack/stack.java
@@ -5,6 +5,7 @@ public class stack{
         while (!s.isEmpty()) {
             System.out.println(s.pop());
         }
+        // System.out.println();
     }
 
     //Q1 - push at bootom of stack
@@ -18,16 +19,47 @@ public class stack{
         s.push(top);
     }
 
-    //Q2
+    //Q2 - reverse a string using stack
+    public static String reverseString(String str){
+        Stack <Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        //reverse
+        StringBuilder result = new StringBuilder();
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
 
-    //Q3
+    //Q3 - reverse a stack
+    public static void reverseStack(Stack <Integer> s){
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
     public static void main(String[] args) {
         Stack <Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
 
-        pushAtBottom(s, 4);
+        // pushAtBottom(s, 4);
+        // printStack(s);
+
+        // String str = "abc";
+        // String result = reverseString(str);
+        // System.out.println(result);
+
+        reverseStack(s);
         printStack(s);
     }
 }
