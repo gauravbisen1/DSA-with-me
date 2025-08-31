3230c97a62f68d51b32aaba73ab6869943ed6938
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-31 13:46:58 +0530

    queue

---
+++ DSA/13.Queue/queueJCF.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class queueJCF {
+    public static void main(String[] args) {
+         //queue jcf
+        Queue<Integer> q  = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
