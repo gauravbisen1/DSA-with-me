7663272869e670ee7c181ca42f9b695e3b5f1cd3
gauravbisen1 <gouravbisen123@gmail.com>
2025-09-04 18:13:55 +0530

    pre post in orders

--- DSA/16.BinaryTree/BinaryTreesB.java
+++ DSA/16.BinaryTree/BinaryTreesB.java
@@ -25,12 +25,62 @@ public class BinaryTreesB{
 
             return newNode;
         }
+
+        //TREE TRAVERSALS
+
+        //a) Preorder
+        public static void preOrder(Node root){
+            //base case
+            if (root == null) {
+                return;
+            }
+            //root
+            System.out.print(root.data + " ");
+            //left subtree
+            preOrder(root.left);
+            //right subtree
+            preOrder(root.right);
+        }
+        //b) Inorder
+        public static void InOrder(Node root){
+            //base case
+            if (root == null) {
+                return;
+            }
+            //left subtree
+            InOrder(root.left);
+            //root
+            System.out.print(root.data + " ");
+            //right subtree
+            InOrder(root.right);
+        }
+        //c) Postorder
+        public static void postOrder(Node root){
+            //base case
+            if (root == null) {
+                return;
+            }
+            //left subtree
+            postOrder(root.left);
+            //right subtree
+            postOrder(root.right);
+            //root
+            System.out.print(root.data + " ");
+        }
+
     }
     public static void main(String[] args) {
         int nodes[] = {1 ,2 ,4 ,-1 ,-1 , 5 ,-1 ,-1 ,3 ,-1 ,6 ,-1 ,-1};
 
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        
+        // System.out.println(root.data);
+
+        // tree.preOrder(root);
+        
+        // tree.InOrder(root);
+        
+        // tree.postOrder(root);
     }
 }
