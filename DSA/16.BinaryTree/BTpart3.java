1b08bf8957a2fc65818812f7d8691351ee91a9d3
gauravbisen1 <gouravbisen123@gmail.com>
2025-09-05 18:02:15 +0530

    LCA

--- DSA/16.BinaryTree/BTpart3.java
+++ DSA/16.BinaryTree/BTpart3.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class BTpart3 {
     static class Node{
         int data;
@@ -10,7 +11,7 @@ public class BTpart3 {
             this.right = null;
         }
     }
-
+// ------------------------------------------------------------------------------------------------
     //K th level of a tree
     public static void kLevel (Node root, int level, int k){
         if (root == null) {
@@ -26,6 +27,74 @@ public class BTpart3 {
         kLevel(root.right, level+1, k);
 
     }
+// ------------------------------------------------------------------------------------------------
+
+    //Lowest common ancestor
+
+    //approch 1
+    //get path
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+    //lowest common ancestor
+    public static Node LCA (Node root, int n1, int n2){
+        ArrayList <Node> path1 = new ArrayList<>();
+        ArrayList <Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        //LCA
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+        //last equal node -> i-1th
+        Node Lca = path1.get(i-1);
+        return Lca;
+    }
+
+    //approch 2
+    public static Node LCA2(Node root, int n1, int n2){
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        //search on left & right subtree
+        Node leftLCA = LCA2(root.left, n1, n2);
+        Node rightLCA = LCA2(root.right, n1, n2);
+
+        //valid value on leftLCA but rightLCA became null
+        if (rightLCA == null) {
+            return leftLCA;
+        }
+        //valid value on rightLCA but leftLCA became null
+        if (leftLCA == null) {
+            return rightLCA;
+        }
+
+        //if both not null
+        return root;
+    }
+
+
+// ------------------------------------------------------------------------------------------------
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -33,8 +102,15 @@ public class BTpart3 {
         root.left.left = new Node(4);
         root.left.right = new Node(5);
         root.right.left = new Node(6);
+        root.right.right = new Node(7);
          
-        int k = 3;
-        kLevel(root, 1, k);
+        // int k = 3;
+        // kLevel(root, 1, k);
+
+        // int n1 = 4 , n2 = 5;
+        // System.out.println(LCA(root, n1, n2).data);
+
+        int n1 = 4 , n2 = 7;
+        System.out.println(LCA2(root, n1, n2).data);
     }
 }
