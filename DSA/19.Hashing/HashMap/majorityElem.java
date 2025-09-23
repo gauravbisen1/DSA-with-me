9cff3de20e6fc8c2611520b254324bf781e0300c
gauravbisen1 <gouravbisen123@gmail.com>
2025-09-23 11:19:24 +0530

    majority element

--- DSA/19.Hashing/HashMap/majorityElem.java
+++ DSA/19.Hashing/HashMap/majorityElem.java
@@ -6,15 +6,21 @@ public class majorityElem{
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < arr.length; i++) {
-            if (map.containsKey(arr[i])) {
-                map.put(arr[i], map.get(arr[i])+1);
-            } else {
-                map.put(arr[i], 1);
-            }
+            // if (map.containsKey(arr[i])) {
+            //     map.put(arr[i], map.get(arr[i])+1);
+            // } else {
+            //     map.put(arr[i], 1);
+            // }
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
 
-        Set <Integer> keySet = map.keySet();
-        for (Integer key : keySet) {
+        // Set <Integer> keySet = map.keySet();
+        // for (Integer key : keySet) {
+        //     if (map.get(key) > arr.length/3) {
+        //         System.out.println(key);
+        //     }
+        // }
+        for (Integer key : map.keySet()) {
             if (map.get(key) > arr.length/3) {
                 System.out.println(key);
             }
